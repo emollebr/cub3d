12d417344314f79014c5bc3d36ea69aa60b82364
Levichul0 <levijimenezrufes@MBPdeLevichul0.fritz.box>
2024-01-12 22:02:06 +0100

    test

--- Cub3d/includes/cub3d.h
+++ Cub3d/includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: levijimenezrufes <levijimenezrufes@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 17:16:37 by lejimene          #+#    #+#             */
-/*   Updated: 2024/01/12 13:11:47 by levijimenez      ###   ########.fr       */
+/*   Updated: 2024/01/12 12:41:50 by levijimenez      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define CUB3D_H
 
 # include "../minilibx_macos/mlx.h"
-#include "../libft/my_lib.h"
+#include "cub3d.h"
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
@@ -22,13 +22,6 @@
 
 # define ROT_SPEED 0.05
 # define MOV_SPEED 0.2
-
-typedef struct s_textures
-{
-    char *type;
-    char *path;
-    struct s_textures *next;
-} t_textures;
 
 typedef struct images
 {
@@ -40,6 +33,12 @@ typedef struct images
 	int		height;
 	int		ret;
 }	t_img;
+
+typedef struct {
+    double r;
+    double g;
+    double b;
+} t_color;
 
 typedef struct s_keys {
     bool w;
@@ -58,7 +57,22 @@ typedef struct s_player {
 	double	dir_y;
 	double	plane_x;
 	double	plane_y;
+	double floor_x;
+    double floor_y;
+	double	old_player_x;
+	double	old_player_y;
 }				t_player;
+
+typedef struct s_texture
+{
+    void *img;
+    char *addr;
+    int bits_per_pixel;
+    int line_length;
+    int endian;
+    int width;
+    int height;
+} t_texture;
 
 typedef struct s_data {
 	void	*img;
@@ -80,35 +94,44 @@ typedef struct s_data {
 	double	jy;
   t_player  player;
   t_keys	keys;
-  t_textures  *textures;
-  int   **worldMap;
-  int   mapHeight;
-  int   mapWidth;
+  t_texture	textures[5];
 }	t_data;
 
 
-typedef struct s_ray {
-    double  camera_x;
-    double  ray_dir_x;
-    double  ray_dir_y;
-    int     map_x;
-    int     map_y;
-    double  side_dist_x;
-    double  side_dist_y;
-    double  delta_dist_x;
-    double  delta_dist_y;
-    double  perp_wall_dist;
-    int     step_x;
-    int     step_y;
-    int     hit;
-    int     side;
-}               t_ray;
 
+typedef struct s_ray
+{
+    double camera_x;
+    double ray_dir_x;
+    double ray_dir_y;
+    int map_x;
+    int map_y;
+    double delta_dist_x;
+    double delta_dist_y;
+    int step_x;
+    int step_y;
+    int side;
+    double side_dist_x;
+    double side_dist_y;
+    int hit;
+    double perp_wall_dist;
+    int line_height;
+    int draw_start;
+    int draw_end;
+    double floor_x;
+    double floor_y;
+    double floor_x_step;
+    double floor_y_step;
+} t_ray;
+
+#define mapWidth 24
+#define mapHeight 24
 #define WIDTH 1024
 #define HEIGHT 720
 
 
-/*int worldMap[mapWidth][mapHeight]=
+
+int worldMap[mapWidth][mapHeight]=
 {
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -134,12 +157,6 @@ typedef struct s_ray {
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-};*/
-
-int   parse_cub_file(const char *filename, t_data *img);
-int    parse_textures(int file, t_data *img);
-char    *get_map_dimensions(int file, t_data *img);
-void    parse_map(int file, char *start_of_map, t_data *img);
-
+};
 
 #endif
